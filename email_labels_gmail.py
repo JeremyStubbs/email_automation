326694b91b2a6a7bd5d57d943a66f1e46506abd4
Jeremy Stubbs <89751851+JeremyStubbs@users.noreply.github.com>
2023-08-08 11:47:13 -0700

    Using app password

--- email_labels_gmail.py
+++ email_labels_gmail.py
@@ -1,67 +1,58 @@
+import os
+from email.message import EmailMessage
+import ssl
+import smtplib
+import mimetypes
+import csv
 
 
-from __future__ import print_function
+body= """
+body  
+"""
 
-import smtplib, ssl
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
+email_sender = 'youremail@gmail.com'
+email_password = 'yourpassword'
+subject = 'Subject'
 
-import os.path
+csv_list_of_recipients = r"C:\Users\jerem\OneDrive\Documents\Family_medicine_residencies.csv"
 
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
+first_path = r"C:\Users\jerem\OneDrive\Documents\Important_Documents\Email_Docs"
+second_path_1 = 'Deans_Statement_SGU.pdf'
+second_path_2="eras_application_2022.pdf"
+second_path_3="MD_Diploma.pdf"
+second_path_4="MSPE.pdf"
+second_path_5="sgu_basic_science.pdf"
 
-# If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+all_attachments = [second_path_1,second_path_2, second_path_3, second_path_4, second_path_5]
 
+context = ssl.create_default_context()
 
-def main():
-    """Shows basic usage of the Gmail API.
-    Lists the user's Gmail labels.
-    """
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
+with smtplib.SMTP_SSL('SMTP.GMAIL.COM', 465, context=context ) as smtp:
+    smtp.login(email_sender, email_password)
+    with open (csv_list_of_recipients, newline='') as csvfile:
+        content = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        for row in content:
+            email_receiver = row
+            em = EmailMessage()
+            em.set_content(body)
+            em['From'] = email_sender
+            em['To']= email_receiver
+            em['Subject']= subject
 
-    try:
-        # Call the Gmail API
-        service = build('gmail', 'v1', credentials=creds)
-        results = service.users().labels().list(userId='me').execute()
-        labels = results.get('labels', [])
-
-        if not labels:
-            print('No labels found.')
-            return
-        print('Labels:')
-        for label in labels:
-            print(label['name'])
-
-    except HttpError as error:
-        # TODO(developer) - Handle errors from gmail API.
-        print(f'An error occurred: {error}')
+            for item in all_attachments:
+                attachment_path = first_path+"/" + item
+                with open(attachment_path, 'rb') as content_file:
+                    content = content_file.read()
+                    em.add_attachment(content, maintype='application', subtype='pdf', filename=item)
 
 
-  
+            smtp.sendmail(email_sender, email_receiver, em.as_string())
 
 
-if __name__ == '__main__':
-    # main()
-    create_message("jeremyb.stubbs@gmail.com", "jeremyb.stubbs@gmail.com", "subject", text)
-    send_message()
+
+
+
+
+
+
+
